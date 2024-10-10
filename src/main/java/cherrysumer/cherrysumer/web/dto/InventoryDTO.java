427a50e2823eda13774687dd34159e60510e211b
오승훈 <shoon0728@naver.com>
2024-10-11 01:43:26 +0900

    [fix] 어노테이션 수정

--- src/main/java/cherrysumer/cherrysumer/web/dto/InventoryDTO.java
+++ src/main/java/cherrysumer/cherrysumer/web/dto/InventoryDTO.java
@@ -2,6 +2,7 @@ package cherrysumer.cherrysumer.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,14 +18,14 @@ public class InventoryDTO {
     @NotEmpty(message = "재고이름을 입력하세요.")
     private String productName;
 
-    @NotEmpty(message = "구매날짜을 입력하세요.")
+    @NotNull(message = "구매날짜을 입력하세요.")
     private LocalDateTime purchase_date;
 
-    @NotEmpty(message = "만료날짜을 입력하세요.")
+    @NotNull(message = "만료날짜을 입력하세요.")
     /*@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")*/
     private LocalDateTime expiration_date;
 
-    @NotEmpty(message = "수량을 입력하세요.")
+    @NotNull(message = "수량을 입력하세요.")
     private int quantity;
 
     private String stockLocation;
--- src/main/java/cherrysumer/cherrysumer/web/dto/ProfileDTO.java
+++ src/main/java/cherrysumer/cherrysumer/web/dto/ProfileDTO.java
@@ -1,6 +1,7 @@
 package cherrysumer.cherrysumer.web.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ public class ProfileDTO {
     private String nickname;
 
     @NotEmpty(message = "이메일을 입력하세요.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
     private String region;  // 현재 거주 지역
