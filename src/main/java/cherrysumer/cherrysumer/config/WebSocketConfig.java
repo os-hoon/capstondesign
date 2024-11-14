f6dceda7ea1822c60e0613f9c9e45c0a56001dd8
sh <dldh0917@naver.com>
2024-11-15 01:02:13 +0900

    [refactor] sockJs 제거

--- src/main/java/cherrysumer/cherrysumer/config/WebSocketConfig.java
+++ src/main/java/cherrysumer/cherrysumer/config/WebSocketConfig.java
@@ -18,8 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // socketJs 클라이언트가 WebSocket 핸드셰이크를 하기 위해 연결할 endpoint를 지정할 수 있다.
         registry.addEndpoint("/chat/inbox")
-                .setAllowedOriginPatterns("*") // cors 허용을 위해 꼭 설정해주어야 한다.
-                .withSockJS();
+                .setAllowedOriginPatterns("*"); // cors 허용을 위해 꼭 설정해주어야 한다.
     }
 
     @Override
