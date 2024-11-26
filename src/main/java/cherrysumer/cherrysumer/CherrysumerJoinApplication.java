8c899cf358b185f351ebd318bafa1e456327515e
sh <dldh0917@naver.com>
2024-11-26 21:21:42 +0900

    [refactor] 시간 수정

--- src/main/java/cherrysumer/cherrysumer/CherrysumerJoinApplication.java
+++ src/main/java/cherrysumer/cherrysumer/CherrysumerJoinApplication.java
@@ -1,8 +1,11 @@
 package cherrysumer.cherrysumer;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +13,11 @@ public class CherrysumerJoinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CherrysumerJoinApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));		// 한국은 "Asia/Seoul"
 	}
 
 }
